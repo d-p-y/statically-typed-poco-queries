3d8def4e669ae0ef93efef09663533c6ac267afb
Dominik Pytlewski <dominik@pytlewski.eu>
2016-11-16 11:25:52 +0100

    AsyncPoco wrappers: initial import

---
+++ StaTypPocoQueries.AsyncPoco/AsyncPocoDatabaseExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using AsyncPoco;
+using StaTypPocoQueries.Core;
+
+namespace StaTypPocoQueries.AsyncPoco {
+    public static class AsyncPocoDatabaseExtensions {
+
+        public static Task<int> DeleteAsync<T>(this Database self, Expression<Func<T, bool>> query) {
+            var translated = ExpressionToSql.Translate(query);
+            return self.DeleteAsync<T>(translated.Item1, translated.Item2);
+        }
+        
+        public static Task<bool> ExistsAsync<T>(this Database self, Expression<Func<T, bool>> query) {
+            var translated = ExpressionToSql.Translate(query);
+            return self.ExistsAsync<T>(translated.Item1, translated.Item2);
+        }
+
+        public static Task<List<T>> FetchAsync<T>(this Database self, Expression<Func<T, bool>> query) {
+            var translated = ExpressionToSql.Translate(query);
+            return self.FetchAsync<T>(translated.Item1, translated.Item2);
+        }
+        
+        public static Task<T> FirstAsync<T>(this Database self, Expression<Func<T, bool>> query) {
+            var translated = ExpressionToSql.Translate(query);
+            return self.FirstAsync<T>(translated.Item1, translated.Item2);
+        }
+
+        public static Task<T> SingleAsync<T>(this Database self, Expression<Func<T, bool>> query) {
+            var translated = ExpressionToSql.Translate(query);
+            return self.SingleAsync<T>(translated.Item1, translated.Item2);
+        }
+        
+        public static Task<int> UpdateAsync<T>(this Database self, Expression<Func<T, bool>> query) {
+            var translated = ExpressionToSql.Translate(query);
+            return self.UpdateAsync<int>(translated.Item1, translated.Item2);
+        }
+    }
+}
