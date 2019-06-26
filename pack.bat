5e6556c1442b4e626938f0746713c11588ee8a80
Dominik Pytlewski <dominik@pytlewski.eu>
2019-06-26 14:09:42 +0200

    pack script updated

--- pack.bat
+++ pack.bat
@@ -5,3 +5,9 @@ cd ..
 cd StaTypPocoQueries.AsyncPoco 
 dotnet pack StaTypPocoQueries.AsyncPoco.csproj -c Release
 cd ..
+
+cd StaTypPocoQueries.AsyncPocoDpy
+dotnet pack StaTypPocoQueries.AsyncPocoDpy.csproj -c Release
+cd ..
+
+echo "exiting"
